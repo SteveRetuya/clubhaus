9ed01503ba280a02508166c3d4917c52ede7f4b6
Shaun Gerald Oka <2240177@slu.edu.ph>
2024-12-06 23:21:03 +0800

    make joined button in forums

--- app/src/main/java/com/example/clubhaus/user/ClubForumsFragment.java
+++ app/src/main/java/com/example/clubhaus/user/ClubForumsFragment.java
@@ -4,8 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
+
+import com.example.clubhaus.MainActivity;
 import com.example.clubhaus.R;
 
 /**
@@ -19,8 +22,21 @@ public class ClubForumsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
+        View view = inflater.inflate(R.layout.fragment_club_forums, container, false);
+        Button joinButton = view.findViewById(R.id.join_button);
+        joinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (joinButton.getText().equals("Join")) {
+                    joinButton.setBackgroundColor(getResources().getColor(R.color.joined));
+                    joinButton.setText("Joined");
+                } else {
+                    joinButton.setBackgroundColor(getResources().getColor(R.color.join));
+                    joinButton.setText("Join");
+                }
+            }
+        });
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_club_forums, container, false);
+        return view;
     }
 }
