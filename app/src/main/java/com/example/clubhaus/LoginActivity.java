2e86e007cbdd28c5eaa8829883ca91f59ce4b67b
Shaun Gerald Oka <2240177@slu.edu.ph>
2024-12-08 19:07:35 +0800

    update SignUpActivity.java and LoginActivity.java secure sign up (no admin making) secure log in (log in with right credentials)

--- app/src/main/java/com/example/clubhaus/LoginActivity.java
+++ app/src/main/java/com/example/clubhaus/LoginActivity.java
@@ -5,12 +5,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.clubhaus.user.HomeFragment;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText emailET, passwordET;
+    String email, password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,9 +36,37 @@ public class LoginActivity extends AppCompatActivity {
         });
 
         loginButton.setOnClickListener(v -> {
+            FirebaseDatabase database = FirebaseDatabase.getInstance("https://clubhaus-37b05-default-rtdb.asia-southeast1.firebasedatabase.app/");
+            emailET = findViewById(R.id.emailInput);
+            passwordET = findViewById(R.id.passwordInput);
+            email = emailET.getText().toString().trim();
+            password = passwordET.getText().toString().trim();
+
+            DatabaseReference reference = database.getReference(email);
+            reference.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if (snapshot.exists()) {
+                        String realPassword = snapshot.child("password").getValue(String.class);
+                        checkPassword(realPassword);
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Email does not exist", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    Toast.makeText(LoginActivity.this, "error", Toast.LENGTH_SHORT).show();
+                }
+            });
+        });
+    }
+
+    protected void checkPassword(String realPassword) {
+        if (password.equals(realPassword)) {
             Intent intent = new Intent(LoginActivity.this, MainActivity.class);
             startActivity(intent);
-        });
-
+        } else {
+            Toast.makeText(LoginActivity.this, "Incorrect password", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/example/clubhaus/SignUpActivity.java
+++ app/src/main/java/com/example/clubhaus/SignUpActivity.java
@@ -17,8 +17,11 @@ import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SignUpActivity extends AppCompatActivity {
     EditText inputEmail, inputUsername, inputBirthday, inputPassword;
@@ -49,8 +52,8 @@ public class SignUpActivity extends AppCompatActivity {
             birthday = inputBirthday.getText().toString().trim();
             password = inputPassword.getText().toString().trim();
 
-            if(email.isEmpty()){
-                Toast.makeText(SignUpActivity.this, "Enter email", Toast.LENGTH_SHORT).show();
+            if(email.isEmpty() || email.equalsIgnoreCase("admin")){
+                Toast.makeText(SignUpActivity.this, "Enter appropriate email", Toast.LENGTH_SHORT).show();
                 return;
             }
 
@@ -69,7 +72,7 @@ public class SignUpActivity extends AppCompatActivity {
                 return;
             }
 
-
+            /*
             mAuth.createUserWithEmailAndPassword(email, password)
                     .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                         @Override
@@ -86,15 +89,28 @@ public class SignUpActivity extends AppCompatActivity {
                             }
                         }
                     });
+             */
 
             FirebaseDatabase database = FirebaseDatabase.getInstance("https://clubhaus-37b05-default-rtdb.asia-southeast1.firebasedatabase.app/");
-            DatabaseReference reference = database.getReference();
+            DatabaseReference reference = database.getReference(email);
+            reference.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if (snapshot.exists()) {
+                        Toast.makeText(SignUpActivity.this, "Email already exists", Toast.LENGTH_SHORT).show();
+                    } else {
+                        DatabaseReference newReference = database.getReference().child(email);
+                        newReference.setValue(new User(username, birthday, password));
+                        startActivity(intent);
+                    }
 
-            DatabaseReference newReference = reference.child(email);
+                }
 
-            newReference.setValue(new User(username, birthday, password));
-
-            startActivity(intent);
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    startActivity(intent);
+                }
+            });
         });
     }
 }
